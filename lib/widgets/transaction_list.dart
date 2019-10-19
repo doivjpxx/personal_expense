40f44d84a1713540c7541e5a828ed0c60aa4487f
Huy Phong <doivjpxx@gmail.com>
2019-10-19 15:13:26 +0700

    improve uii

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -30,38 +30,26 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (BuildContext context, int index) {
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.symmetric(
-                            vertical: 10.0, horizontal: 15.0),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Theme.of(context).primaryColor,
-                                width: 2)),
-                        child: Text(
-                          '\$${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16.0),
+                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FittedBox(
+                          child: Text(
+                              '\$${transactions[index].amount.toStringAsFixed(2)}'),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            transactions[index].title,
-                            style: Theme.of(context).textTheme.title,
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(transactions[index].date),
-                            style: TextStyle(color: Colors.grey),
-                          )
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: TextStyle(color: Colors.grey),
+                    ),
                   ),
                 );
               },
@@ -70,3 +58,40 @@ class TransactionList extends StatelessWidget {
     );
   }
 }
+
+// return Card(
+//                   child: Row(
+//                     children: <Widget>[
+//                       Container(
+//                         padding: EdgeInsets.all(10),
+//                         margin: EdgeInsets.symmetric(
+//                             vertical: 10.0, horizontal: 15.0),
+//                         decoration: BoxDecoration(
+//                             border: Border.all(
+//                                 color: Theme.of(context).primaryColor,
+//                                 width: 2)),
+//                         child: Text(
+//                           '\$${transactions[index].amount.toStringAsFixed(2)}',
+//                           style: TextStyle(
+//                               color: Theme.of(context).primaryColor,
+//                               fontWeight: FontWeight.bold,
+//                               fontSize: 16.0),
+//                         ),
+//                       ),
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: <Widget>[
+//                           Text(
+//                             transactions[index].title,
+//                             style: Theme.of(context).textTheme.title,
+//                           ),
+//                           Text(
+//                             DateFormat.yMMMd().format(transactions[index].date),
+//                             style: TextStyle(color: Colors.grey),
+//                           )
+//                         ],
+//                       )
+//                     ],
+//                   ),
+//                 );
+//               },
